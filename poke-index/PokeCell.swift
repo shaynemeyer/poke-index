f4e652cdb32f0f414d986d614e41da75a4d4ad4d
shaynemeyer <shayne.meyer@gmail.com>
2015-12-30 21:28:46 -0800

    got pokemons displaying in the collection view. Only shows the images right now. will add the names soon.

---
+++ poke-index/PokeCell.swift
@@ -0,0 +1,31 @@
+//
+//  PokeCell.swift
+//  poke-index
+//
+//  Created by Shayne on 12/30/15.
+//  Copyright © 2015 Maynesoft LLC. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalizedString
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+        
+    }
+}
