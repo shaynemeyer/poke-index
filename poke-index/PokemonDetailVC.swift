3d5a23ebc982dd9e5b926560468b8637946f91cc
shaynemeyer <shayne.meyer@gmail.com>
2015-12-31 14:27:02 -0800

    added PokemonDetailVC.

---
+++ poke-index/PokemonDetailVC.swift
@@ -0,0 +1,20 @@
+//
+//  PokemonDetailVC.swift
+//  poke-index
+//
+//  Created by Shayne on 12/31/15.
+//  Copyright © 2015 Maynesoft LLC. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+}
