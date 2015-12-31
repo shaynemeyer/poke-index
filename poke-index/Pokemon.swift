f7d05f61077350971120f1981d4ffebeec00ecd0
shaynemeyer <shayne.meyer@gmail.com>
2015-12-30 20:22:09 -0800

    added Pokemon class.

---
+++ poke-index/Pokemon.swift
@@ -0,0 +1,28 @@
+//
+//  Pokemon.swift
+//  poke-index
+//
+//  Created by Shayne on 12/30/15.
+//  Copyright © 2015 Maynesoft LLC. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+}
