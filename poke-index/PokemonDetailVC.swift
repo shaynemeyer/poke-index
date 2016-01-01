c2bda616dc715c9128537091a8c0d7b63d00980a
shaynemeyer <shayne.meyer@gmail.com>
2015-12-31 20:52:16 -0800

    updated pokemon class.

--- poke-index/PokemonDetailVC.swift
+++ poke-index/PokemonDetailVC.swift
@@ -11,6 +11,18 @@ import UIKit
 class PokemonDetailVC: UIViewController {
 
     @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var pokeindexLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var attackLbl: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    @IBOutlet weak var evoLbl: UILabel!
+    
     var pokemon: Pokemon!
     
     override func viewDidLoad() {
@@ -20,4 +32,7 @@ class PokemonDetailVC: UIViewController {
     }
 
 
+    @IBAction func backBtnPressed(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 }
